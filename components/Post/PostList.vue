bdaf9f2831138db819d1c60b73054f5c53def629
Cryptouru <cryptouru@gmail.com>
2018-09-01 18:28:03 -0300

    Added PostList component

---
+++ components/Post/PostList.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <v-card class="loading-item" v-if="loading" v-for="(n, index) in 5" :key="index">
+      <div class="glow-title">
+        loading title
+      </div>
+      <div class="glow-text">
+        <span>Loading</span> <span>cool</span>
+      </div>
+      <div class="glow-box" />
+      <div class="glow-title">
+        loading title
+      </div>
+      <div class="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </div>
+    </v-card>
+    <div v-if="noPosts && !this.loading">
+      There are no posts for this tag
+    </div>
+    <div v-if="showPosts">
+      <post-preview 
+        class="post-preview"
+        v-for="(post, index) in posts" 
+        :key="index" 
+        v-bind="post" 
+        @vote="$emit('vote', $event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import PostPreview from "./PostPreview";
+export default {
+  name: "PostList",
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    posts: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  components: {
+    PostPreview
+  },
+  created () {
+    console.log(this.posts)
+    console.log(this.noPosts)
+    console.log(this.showPosts)
+  },
+  computed: {
+    noPosts() {
+      return this.posts.length === 0;
+    },
+    showPosts() {
+      return !this.loading && !this.noPosts;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.post-preview {
+  margin-top: 10px;
+}
+.loading-item .glow-box, .loading-item .glow-text span, .loading-item .glow-title {
+    margin: 16px 0;
+    animation: glow 1.5s ease-in-out infinite;
+    background: #eee;
+    color: transparent;
+    cursor: progress;
+}
+.loading-item .glow-box {
+    width: 100%;
+    height: 120px;
+    display: block;
+}
+.loading-item {
+    width: 100%;
+    background: white;
+    line-height: 1rem;
+    padding: 16px;
+    margin-bottom: 32px; 
+}
+</style>
---
+++ stories/PostList.stories.js
@@ -0,0 +1,83 @@
+import Vue from 'vue'
+
+import { storiesOf } from '@storybook/vue'
+import { action } from "@storybook/addon-actions"
+
+
+import PostList from '../components/Post/PostList.vue'
+
+export const posts = [
+  {
+    title: 'The Menzoberranzan Bank of Neoxian: daily contracts: 2018-08-30',
+    snippet: 'The Bank of Neoxian shall loan the sum of 120 steem to @owner99. He promises to repay 135 steem. He shall p...',
+    author: 'neoxian',
+    author_rep: 68,
+    payout: 12.17,
+    vote_count: 19,
+    comment_count: 19,
+    parent_permlink: 'bank',
+    root_permlink: 'the-menzoberranzan-bank-of-neoxian-daily-contracts-2018-08-30',
+    img_url: 'https://steemitimages.com/0x0/https://userscontent2.emaze.com/images/20793e54-4ec0-4009-91c3-fb135c1e4fcb/471794c96ca375908b6f64336e5a6019.jpg',
+    created: new Date(2018, 0, 1, 9, 0)
+  },
+  {
+    title: 'The Menzoberranzan Bank of Neoxian: daily contracts: 2018-08-30',
+    snippet: 'The Bank of Neoxian shall loan the sum of 120 steem to @owner99. He promises to repay 135 steem. He shall p...',
+    author: 'neoxian',
+    author_rep: 68,
+    payout: 12.17,
+    vote_count: 19,
+    comment_count: 19,
+    parent_permlink: 'bank',
+    root_permlink: 'the-menzoberranzan-bank-of-neoxian-daily-contracts-2018-08-30',
+    img_url: 'https://steemitimages.com/0x0/https://userscontent2.emaze.com/images/20793e54-4ec0-4009-91c3-fb135c1e4fcb/471794c96ca375908b6f64336e5a6019.jpg',
+    created: new Date(2018, 0, 1, 9, 0)
+  },
+  {
+    title: 'The Menzoberranzan Bank of Neoxian: daily contracts: 2018-08-30',
+    snippet: 'The Bank of Neoxian shall loan the sum of 120 steem to @owner99. He promises to repay 135 steem. He shall p...',
+    author: 'neoxian',
+    author_rep: 68,
+    payout: 12.17,
+    vote_count: 19,
+    comment_count: 19,
+    parent_permlink: 'bank',
+    root_permlink: 'the-menzoberranzan-bank-of-neoxian-daily-contracts-2018-08-30',
+    img_url: 'https://steemitimages.com/0x0/https://userscontent2.emaze.com/images/20793e54-4ec0-4009-91c3-fb135c1e4fcb/471794c96ca375908b6f64336e5a6019.jpg',
+    created: new Date(2018, 0, 1, 9, 0),
+    voted:true
+  }
+];
+
+
+export const methods = {
+  vote: action("vote")
+};
+
+
+const paddedList = () => {
+  return {
+    template: '<div style="padding: 3rem;"><story/></div>'
+  };
+};
+
+storiesOf('PostList', module)
+  .addDecorator(paddedList)
+  .add('default', () => ({
+    components: { PostList },
+    template: '<PostList  :posts="posts" @vote="vote"/>',
+    data: () => ({ posts: posts }),
+    methods
+  }))
+  .add('loading', () => ({
+    components: { PostList },
+    template: '<PostList  loading/>'
+  }))
+  /*.add("voted", () => {
+    return {
+      components: { PostList },
+      template: '<PostList  v-bind="posts" @vote="vote"/>',
+      data: () => ({ preview: { ...preview, voted: true } }),
+      methods
+    };
+  })*/
