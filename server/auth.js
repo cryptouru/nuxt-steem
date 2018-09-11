bb1557c66ec6015a4a81466298ec1a74179dc4da
Cryptouru <cryptouru@gmail.com>
2018-09-11 18:49:55 -0300

    Serverside auth Init

---
+++ server/auth.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let steemConnect = require('../helpers/steemconnect')
+let router = express.Router()
+
+/* GET authenticate a user/redirect to steemconnect. */
+router.get('/', (req, res, next) => {
+  if (!req.query.access_token ) {
+      let uri = steemConnect.getLoginURL();
+      res.redirect(uri);
+  } else {
+    steemConnect.setAccessToken(req.query.access_token);
+    steemConnect.me((err, steemResponse) => {
+        req.session.authUser = steemResponse.account;
+        req.session.access_token = req.query.access_token;
+        res.redirect(`/@${req.session.authUser.name}`)
+      })
+  }
+})
+
+// GET `/api/logout` to log out the user and remove it from the `req.session`
+router.get('/logout', function (req, res) {
+  delete req.session.authUser
+  delete req.session.access_token
+  res.json({ ok: true })
+})
+
+module.exports = router
--- server/index.js
+++ server/index.js
@@ -1,22 +1,40 @@
 
 const express = require('express')
 const { Nuxt, Builder } = require('nuxt')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 const app = express()
+const config = require('../config')
+const auth = require('./auth')
+
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
 
+// Body parser, to access `req.body`
+app.use(bodyParser.json())
+
+// Sessions to create `req.session`
+app.use(session({
+  secret: config.session.secret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } // One week
+}))
+
+app.use('/api/auth', auth)
+
 // Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'production')
+let nuxtConfig = require('../nuxt.config.js')
+nuxtConfig.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
   // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(nuxtConfig)
 
   // Build only in dev mode
-  if (config.dev) {
+  if (nuxtConfig.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
